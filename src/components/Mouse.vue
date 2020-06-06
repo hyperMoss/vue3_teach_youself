fad41c2c55c3e54628f1aa26875479f0640feb9e
gooingyu <gooingyu@outlook.com>
2020-06-06 12:06:42 +0800

    feat 添加 ref 和reactive用例

--- src/components/Mouse.vue
+++ src/components/Mouse.vue
@@ -1,23 +1,39 @@
 <!--
  * @Author: Dongzy
  * @since: 2020-06-05 17:38:46
- * @lastTime: 2020-06-05 17:43:53
+ * @lastTime: 2020-06-06 12:05:43
  * @LastAuthor: Dongzy
- * @FilePath: \vite-test\src\components\Mouse.vue
+ * @FilePath: \vue3_teach_youself\src\components\Mouse.vue
  * @message: 
 --> 
 <template
-  ><div>x:{{ x }}y:{{ y }}</div>
+  >
+  <div>Ref</div>
+  <div>x:{{ x }}y:{{ y }}</div>
+ <div>Reactive</div>
+<div>x:{{ pos.j }}y:{{ pos.k }}</div>
+ <div>Reactive 标准类型</div>
+<div>x:{{j }}y:{{ k }}</div>
+<div>Reactive 深拷贝</div>
+<div>x:{{posDeep.j}}y:{{ posDeep.k }}</div>
+<div>Reactive+ToRef</div>
+<div>x:{{m}}y:{{ n }}</div>
 </template>
 
 <script>
-import { useMousePosition } from "./mouseFunc/index";
+// 将之前的函数引入组件
+import { useMousePosition,useMousePositionReactive,useMousePositionToRefs } from "./mouseFunc/index";
 
 export default {
+  // 将其放入setup 装载区间
   setup() {
     const { x, y } = useMousePosition();
+    const pos = useMousePositionReactive();
+    const {j,k} = useMousePositionReactive();
+    const posDeep={...useMousePositionReactive()}
+    const {m,n}= useMousePositionToRefs()
     // 其他逻辑...
-    return { x, y };
+    return { x, y,pos,k,j,posDeep,m,n };
   }
 };
 </script>
--- src/components/mouseFunc/index.js
+++ src/components/mouseFunc/index.js
@@ -1,30 +1,69 @@
 /*
  * @Author: Dongzy
  * @since: 2020-06-05 13:40:35
- * @lastTime: 2020-06-05 13:53:20
+ * @lastTime: 2020-06-06 12:04:44
  * @LastAuthor: Dongzy
- * @FilePath: \my-test3\src\components\mouseFunc\index.js
+ * @FilePath: \vue3_teach_youself\src\components\mouseFunc\index.js
  * @message:
  */
 
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, reactive, ref, toRefs } from 'vue';
 
 export function useMousePosition() {
-  const x = ref(0);
-  const y = ref(0);
+	// 使用ref 去包裹变量x，y使其变动可被监听到
+	const x = ref(0);
+	const y = ref(0);
+	// 定义更新事件函数
+	function update(e) {
+		x.value = e.pageX;
+		y.value = e.pageY;
+	}
+	// 绑定鼠标移动事件
+	onMounted(() => {
+		window.addEventListener('mousemove', update);
+	});
+	// 移除绑定鼠标移动事件
+	onUnmounted(() => {
+		window.removeEventListener('mousemove', update);
+	});
 
-  function update(e) {
-    x.value = e.pageX;
-    y.value = e.pageY;
-  }
+	return { x, y };
+}
+export function useMousePositionReactive() {
+	// 使用ref 去包裹变量x，y使其变动可被监听到
+	const pos = reactive({ j: 0, k: 0 });
+	// 定义更新事件函数
+	function update(e) {
+		pos.j = e.pageX;
+		pos.k = e.pageY;
+	}
+	// 绑定鼠标移动事件
+	onMounted(() => {
+		window.addEventListener('mousemove', update);
+	});
+	// 移除绑定鼠标移动事件
+	onUnmounted(() => {
+		window.removeEventListener('mousemove', update);
+	});
 
-  onMounted(() => {
-    window.addEventListener("mousemove", update);
-  });
+	return  pos ;
+}
+export function useMousePositionToRefs() {
+	// 使用ref 去包裹变量x，y使其变动可被监听到
+	const pos = reactive({ m: 0, n: 0 });
+	// 定义更新事件函数
+	function update(e) {
+		pos.m = e.pageX;
+		pos.n = e.pageY;
+	}
+	// 绑定鼠标移动事件
+	onMounted(() => {
+		window.addEventListener('mousemove', update);
+	});
+	// 移除绑定鼠标移动事件
+	onUnmounted(() => {
+		window.removeEventListener('mousemove', update);
+	});
 
-  onUnmounted(() => {
-    window.removeEventListener("mousemove", update);
-  });
-
-  return { x, y };
+	return toRefs(pos)  ;
 }
